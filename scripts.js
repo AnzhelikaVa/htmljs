59ccd7ac68fae28448502a590a971fe26ce39dd2
anzhe <anzhelikavas@icloud.com>
2018-07-31 19:42:34 -0700

    tytle Calculator

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-function sayHello() {
-    const title = document.getElementsByTagName('h1');
-    console.log(title[0]);
-
+function clear() {
+    document.getElementById('amount').value = '_';
 }
