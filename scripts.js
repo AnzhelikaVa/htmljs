de306f5f960c9a1baafc436830e682c1b43d9588
anzhe <anzhelikavas@icloud.com>
2018-08-02 23:05:18 -0700

    add function digit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
-function clear() {
-    document.getElementById('amount').value = '_';
+let firstValue = '';
+
+function digit(n) {
+    console.log("Works! " + n);
+    firstValue += n;
+    write(firstValue);
+}
+ function write(n){
+    document.getElementsById('display').value = n;
 }
